fd3f5bc27f480a0774c3a0dcab236c160072b058
Mohammad Badruzzaman <mohammad.badruzz@gmail.com>
2020-02-07 18:14:21 -0500

    fixed buttons

--- JS/app.js
+++ JS/app.js
@@ -1,24 +1,86 @@
-let x = "rock";
-let y = "paper";
-let z = "sissors";
-const input = " ";
-const userInput = $(input[type = "text"]).val()
-
-function logic (){
-  if(x === y){
-    console.log("papers wins")
-  }
-  else if (x === z){
-    console.log("rock wins")
-  }
-  else if (y === z){
-    console.log("sissors wins")
-  }
-}
-
-
 $(document).ready(function(){
   $("button").click(function(){
     $(".test").hide();
   });
 });
+
+class player1{
+constructor(){
+
+ this.hp = 20;
+const input = " ";
+const userInput = $(input[type = "button"]).val();
+
+
+  }
+
+}
+
+class player2{
+constructor(){
+
+const hp= 20;
+const input = " ";
+const userInput = $(input[type = "button"]).val();
+  }
+
+}
+
+
+ $( "#element1" ).click(function() {
+   console.log( "element1" );
+
+   let hpToTakeAway = "";
+   let attackDamage = "";
+
+  });
+
+  $( "#element2" ).click(function() {
+   console.log( "element2" );
+
+   let hpToTakeAway = "";
+   let attackDamage = "";
+
+  });
+
+   $( "#element3" ).click(function() {
+   console.log( "element3" );
+
+   let hpToTakeAway = "";
+   let attackDamage = "";
+
+  });
+
+    $( "#element4" ).click(function() {
+   console.log( "element4" );
+
+   let hpToTakeAway = "";
+   let attackDamage = "";
+
+  });
+
+   $( "#element5" ).click(function() {
+   console.log( "element5" );
+
+   let hpToTakeAway = "";
+   let attackDamage = "";
+
+  });
+
+
+
+
+// function logic (){
+//   if(x === y){
+//     console.log("papers wins")
+//   }
+//   else if (x === z){
+//     console.log("rock wins")
+//   }
+//   else if (y === z){
+//     console.log("sissors wins")
+//   }
+// }
+
+
+
