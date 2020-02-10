bd165bdcc778c7a2b023391ef6322f998dc30843
Mohammad Badruzzaman <mohammad.badruzz@gmail.com>
2020-02-10 15:16:19 -0500

    char classes

---
+++ JS/char.js
@@ -0,0 +1,28 @@
+let char;
+
+function Cryomancer(name,health,strength,speed){
+  this.health = health;
+  this.speed = speed;
+  this.strength = strength;
+}
+
+function Pyromancer(name,health,strength,speed){
+  this.health = health;
+  this.speed = speed;
+  this.strength = strength;
+}
+
+function Gryomancer(name,health,strength,speed){
+  this.health = health;
+  this.speed = speed;
+  this.strength = strength;
+}
+
+
+function Zen(name,health,strength,speed){
+  this.health = health;
+  this.speed = speed;
+  this.strength = strength;
+}
+
+// let char = new Character("Cyromancer" , 100 );
